41d119ec22d46b09d2113d4b8118e784d7d3ef4d
Cale McCollough <cale.mccollough@gmail.com>
2022-08-15 10:31:39 -0700

    Add Chrome Extension boilerplate with Manifest v3 and Show Facebook Computer Vision Tags Chrome Extension code #3

--- Extension/Background.js
+++ Extension/Background.js
@@ -5,7 +5,7 @@ chrome.alarms.create({
 
 chrome.alarms.onAlarm.addListener((alarm) => {
   chrome.storage.local.get(["timer", "is_running"], (res) => {
-    console.log("background")
+    console.log("tick")
     const time = res.timer ?? 0
     is_running = res.is_running ?? true
     if(!is_running) {
@@ -20,8 +20,8 @@ chrome.alarms.onAlarm.addListener((alarm) => {
     chrome.storage.sync.get(["notification_time"], (res) => {
       notification_time = res.notification_time 
       if(time % notification_time == 0) {
-        this.registration.showNotification("AStartup MCC Task Timer", {
-          body: `${notification_time} second since I banged your mom last.`,
+        this.registration.showNotification("Timer up!", {
+          body: `${notification_time}.`,
           icon: "icon128.png"
         })
       }
